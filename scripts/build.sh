2fc8d33e7973a6132e6ccf2f7649834e2c6104f1
Eduardo Garcia <eduardo.garcia@esailors.de>
2017-10-23 23:35:19 +0200

    Add Jekyll gem, update build process to run Jekyll, copy compiled CSS

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+git checkout -B tmp
+rm -rf _site
+bundle exec jekyll build
+
 sass --style compressed assets/main.scss assets/main.min.css
+cp assets/main.min.css assets/main.min.css.map _site/assets
