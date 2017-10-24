3b87156c77f86c40a15b68db18fa1f4cfbd99262
Eduardo Garcia <eduardo.garcia@esailors.de>
2017-10-24 20:46:52 +0200

    Add “[ci skip]” to release commits

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git add --force _site
-git commit -m "Release `date '+%Y-%m-%d %H:%M'`"
+git commit -m "Release `date '+%Y-%m-%d %H:%M'` [ci skip]"
 git branch -D gh-pages
 git subtree split --prefix _site -b gh-pages
 git push -f origin gh-pages:gh-pages
