3e072ec112223d278a22aea7f9ad41e4eff4cf43
Eduardo Garcia <eduardo.garcia@esailors.de>
2017-11-01 20:33:02 +0100

    Add cache busting to CSS files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+export CURRENT_SHA="$(git log --pretty=format:'%h' -n 1)"
+
+sed -i -e "s/assets\/main.css/assets\/main-$CURRENT_SHA.css/g" index.html _includes/character.html
+
 git checkout -B tmp
 rm -rf _site
 bundle exec jekyll build
 
-sass --style compressed assets/main.scss assets/main.css
-cp assets/main.css assets/main.css.map _site/assets
+sass --style compressed assets/main.scss "assets/main-$CURRENT_SHA.css"
+cp assets/*.css* _site/assets
