eb67db984a1a0afd4850a4ba956ad0aa52b0e4bb
Eduardo Garcia <eduardo.garcia@esailors.de>
2017-11-10 18:17:46 +0100

    Add fingerprinting for JS files, document build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,18 @@
 
 export CURRENT_SHA="$(git log --pretty=format:'%h' -n 1)"
 
+# Add fingerprinting to CSS and JS files
 sed -i -e "s/assets\/main.css/assets\/main-$CURRENT_SHA.css/g" index.html _includes/character.html
+sed -i -e "s/assets\/main.js/assets\/main-$CURRENT_SHA.js/g" _includes/character.html
 
+# Build Jekyll site
 git checkout -B tmp
 rm -rf _site
 bundle exec jekyll build
 
+# Compile SASS assets, rename CSS output files and copy them to _site dir
 sass --style compressed assets/main.scss "assets/main-$CURRENT_SHA.css"
 cp assets/*.css* _site/assets
+
+# Rename JS files and copy them to _site dir
+cp assets/main.js "_site/assets/main-$CURRENT_SHA.js"
