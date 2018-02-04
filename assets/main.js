54ddf35371b532cb299a9582d092688a8143069c
Eduardo Garcia <eduardo.garcia@esailors.de>
2018-02-04 16:53:42 +0100

    (chocolate) Add speech synthesis
    Also add page-specific init method call, change to multiple language ids
    and use the first ones with an available voice.

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function (event) {
   FeatureDetection.init();
-  Caracteres.init();
+
+  switch (document.body.id) {
+    case 'caracteres':
+      Caracteres.init();
+      break;
+
+    case 'chocolate':
+      Chocolate.init();
+      break;
+
+    default:
+      console.warn('Unknown body id "' + document.body.id + '", no init function executed.');
+      break;
+  }
 });
 
 var FeatureDetection = (function () {
@@ -72,4 +85,57 @@ var Caracteres = (function () {
   };
 
   return new Caracteres();
+})();
+
+var Chocolate = (function () {
+  function Chocolate() {
+    this.voices = {};
+  }
+
+  Chocolate.prototype.init = function () {
+    if (FeatureDetection.hasSpeechSynthesis()) {
+      speechSynthesis.onvoiceschanged = this.onVoicesChanged;
+    }
+  };
+
+  Chocolate.prototype.onVoicesChanged = function () {
+    speechSynthesis.getVoices().forEach(function (voice) {
+      if (!window.Chocolate.voices[voice.lang]) {
+        window.Chocolate.voices[voice.lang] = voice;
+      }
+    });
+
+    window.Chocolate.addSpeakButtonClickListeners();
+  };
+
+  Chocolate.prototype.onSpeakButtonClick = function () {
+    window.Chocolate.speak(this.getAttribute('data-word'), this.getAttribute('data-language-id'));
+  };
+
+  Chocolate.prototype.addSpeakButtonClickListeners = function () {
+    Array.prototype.slice.apply(document.querySelectorAll('[data-word]'))
+      .forEach(function (button) {
+        var languageIds = button.getAttribute('data-language-ids').split(',');
+
+        for (var i = 0; i < languageIds.length; i++) {
+          if (window.Chocolate.voices[languageIds[i]]) {
+            button.href = 'javascript:void(0);';
+            button.target = '';
+            button.setAttribute('data-language-id', languageIds[i]);
+            button.addEventListener('click', window.Chocolate.onSpeakButtonClick);
+
+            break;
+          }
+        }
+      });
+  };
+
+  Chocolate.prototype.speak = function (word, language) {
+    var utterance = new SpeechSynthesisUtterance(word);
+    utterance.voice = window.Chocolate.voices[language];
+    utterance.lang = language;
+    speechSynthesis.speak(utterance);
+  };
+
+  return new Chocolate();
 })();
